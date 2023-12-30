1137fa6020d568ae616bc731e69ba5fc48f8f009
Marcelo Primo <linux-user@486DX>
2023-12-30 08:09:01 -0300

    feat: arquivos aula pacotes

---
+++ 1 - pacotes/auxiliar/auxiliar.go
@@ -0,0 +1,7 @@
+package auxiliar
+
+import "fmt"
+
+func Escrever() {
+	fmt.Println("Texto do pacote auxiliar.")
+}
---
+++ 1 - pacotes/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"modulo/auxiliar"
+)
+
+func main() {
+	fmt.Println("Texto arquivo main.")
+	auxiliar.Escrever()
+}
